a6defe7581adce9cc9ac6a01b0c20166d685472e
Hung Le <hungle913@gmail.com>
2020-05-21 14:41:37 -0700

    setup file structure for new project

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
