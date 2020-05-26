3341baf7b18f8a27ab5949c00b4026baabd45009
Hung Le <hungle913@gmail.com>
2020-05-26 14:01:30 -0700

    added side nav function to index and saved handlebars. Added on click button to save articles in index.js

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.sidenav').sidenav();
+
+	// Save article button changes the saved property of the article model from false to true
+	$(document).on("click", "#saveArticle", function() {
+		// Get article id
+		let articleID = $(this).parent().attr("data-id");
+		console.log(articleID);
+		// Run a POST request to update the article to be saved
+	  $.ajax({
+	    method: "POST",
+	    url: "/save/" + articleID,
+	    data: {
+	      saved: true
+	    }
+	  })
+	  location.reload(true);
+	});
+
+
+});
