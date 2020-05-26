ce6a8ba5888b13c99713d12cd1969e667f90d4d0
Hung Le <hungle913@gmail.com>
2020-05-26 14:33:44 -0700

    fixed spacing in index and saved handlebars. Corrected button names for saved page. Added route for saved page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -27,13 +27,26 @@ module.exports = function(app) {
   });
 
   app.get("/saved", function (req, res) {
-    // db.Article.find({ "saved": true }).populate("notes").exec(function (error, articles) {
-    //   let newsArticles = {
-    //     article: articles
-    //   };
-    //   res.render("saved", newsArticles);
-    // });
-    res.render("saved");
+    db.Article.find({ "saved": true })
+    .then(function(dbArticle) {
+      // console.log(dbArticle)
+      var x = dbArticle.map(function(article) { 
+        return { 
+          id: article._id, 
+          title: article.title,
+          summary: article.summary,
+          link: article.link
+        };
+      });
+      // If we were able to successfully find Articles, send them back to the client
+      res.render("saved", {
+        article: x
+      });
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.render("404")
+    });
   });
 
   // Render 404 page for any unmatched routes
