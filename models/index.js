ea7a68b3be3e6408ef30d371f880e684e85eff5e
Hung Le <hungle913@gmail.com>
2020-05-30 14:22:54 -0700

    changed model names

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Note: require("./nnote"),
-    Article: require("./aarticle")
+    Note: require("./note"),
+    Article: require("./article")
   };
   
